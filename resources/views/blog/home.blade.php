39b7e0090ebae94c7a8255e4d4bd81d1ea5ce64a
Agustin <agustinbonillaeguren@gmail.com>
2022-09-12 20:29:54 -0300

    Se pasa todo a un commit debido a problema

---
+++ resources/views/blog/home.blade.php
@@ -0,0 +1,9 @@
+@include('headerGeneral', ['Section' => 'Inicio'])
+
+<h1>Blogs</h1>
+@foreach ($blogs as $blog)
+    <h2><a href="{{ url()->route('blog.index', $blog->id) }}">{{ $blog->title }}</a></h2>
+    <p>{{ $blog->author }}</p>
+    <p>{{ $blog->body }}</p>
+    <hr>
+@endforeach
---
+++ routes/web.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::redirect('/', '/auth/login', 301);
+
+Route::get('/auth/register', function () { return view('user.register'); })
+-> name("register")
+-> middleware("guest");
+
+Route::post('/auth/register', [UserController::class, "Register"])
+-> name("register")
+-> middleware("guest");
+
+Route::get('/auth/login', function () { return view('user.login'); })
+-> name("login")
+-> middleware("guest");
+
+Route::post('/auth/login', [UserController::class, "Login"])
+-> name("login")
+-> middleware("guest");
+
+Route::get('/user/modify', function () { return view('user.profile'); })
+-> name("profile")
+-> middleware("auth");
+
+Route::post('/user/modify', [UserController::class, "Modify"])
+-> name("profile")
+-> middleware("auth");
+
+Route::get('/user/delete', function() { return view('user.deleteuser'); })
+-> name("deleteUser")
+-> middleware("auth");
+
+Route::post('/user/delete', [UserController::class, "Delete"])
+-> name("deleteUser")
+-> middleware("auth");
+
+Route::get('/user/logout', [UserController::class, "Logout"])
+-> name("logout")
+-> middleware("auth");
+
+Route::get('/blog', [PostController::class, "getPosts"])
+-> name("home")
+-> middleware("auth");
+
+Route::get('/blog/add', function () { return view('blog.addPost'); })
+-> name("blog.add")
+-> middleware("auth");
+
+Route::post('/blog/add', [PostController::class, 'Create'])
+-> name("blog.add")
+-> middleware("auth");
+
+Route::get('/blog/edit/{index}', function () { return view('blog.addPost'); })
+-> name("blog.edit")
+-> middleware("auth");
+
+Route::post('/blog/edit', [PostController::class, 'Edit'])
+-> name("blog.edit")
+-> middleware("auth");
+
+#/blog/delete
+
+Route::get('/blog/myposts', [PostController::class, "getMyPosts"])
+-> name("myPosts")
+-> middleware("auth");
+
+Route::get('/blog/{index}', [PostController::class, "getPost"])
+-> name("blog.index")
+-> middleware("auth");
